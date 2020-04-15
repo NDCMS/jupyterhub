79cafc78c3ae5516fe7191a3cb11780e95bbec5a
Kenyi Hurtado <khurtado@nd.edu>
2020-04-15 09:39:31 -0400

    Fix index filename

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,14 +24,13 @@ author = 'Kenyi Hurtado'
 # The full version, including alpha/beta/rc tags
 release = 'v0.1'
 
-
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark'
-]
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
